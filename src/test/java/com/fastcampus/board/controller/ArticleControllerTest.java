c0f5e7057c6145016ba33941328845320076065d
Hyeonhui <hh.blaire.cho@gmail.com>
2024-08-11 20:54:12 +0900

    #5 Article Controller TDD. 테스트 골격 잡기.

---
+++ src/test/java/com/fastcampus/board/controller/ArticleControllerTest.java
@@ -0,0 +1,81 @@
+package com.fastcampus.board.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@DisplayName("뷰 컨트롤러 - 게시글")
+@WebMvcTest(ArticleController.class)
+class ArticleControllerTest {
+
+    private final MockMvc mvc;
+
+    public ArticleControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @Disabled("구현 중")
+    @DisplayName("[view][GET] 게시판 페이지 - 정상 호출")
+    @Test
+    public void test_ArticlesViewRequest() throws Exception {
+        // Given Nothing
+        // When Request Articles View
+        // Then Returns That View
+        mvc.perform(get("/articles"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+            .andExpect(model().attributeExists("articles"));
+    }
+
+    @Disabled("구현 중")
+    @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
+    @Test
+    public void test_ArticleViewRequest() throws Exception {
+        // Given Nothing
+        // When Request Article View
+        // Then Returns That View
+        mvc.perform(get("/articles/1"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+            .andExpect(view().name("articles/detail"))
+            .andExpect(model().attributeExists("article"))
+            .andExpect(model().attributeExists("articleComments"));
+    }
+
+    @Disabled("구현 중")
+    @DisplayName("[view][GET] 게시글 검색 전용 페이지 - 정상 호출")
+    @Test
+    public void test_ArticleSearchViewRequest() throws Exception {
+        // Given Nothing
+        // When Request ArticleSearch View
+        // Then Returns That View
+        mvc.perform(get("/articles/search"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+            .andExpect(model().attributeExists("articles/search"));
+    }
+
+    @Disabled("구현 중")
+    @DisplayName("[view][GET] 게시글 해시태그 검색 페이지 - 정상 호출")
+    @Test
+    public void test_HashTagSearchViewRequest() throws Exception {
+        // Given Nothing
+        // When Request ArticleHashTagSearch View
+        // Then Returns That View
+        mvc.perform(get("/articles/search-hashtag"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+            .andExpect(model().attributeExists("articles/search-hashtag"));
+    }
+
+}
