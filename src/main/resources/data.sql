5f0aeba31b5e78d9c9a2d43be05ff53b983d9023
Blaire <113817141+hh-blaire-cho@users.noreply.github.com>
2024-08-12 07:47:37 +0900

    Revert "#14 userId->username"

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
 -- 테스트 계정
 -- TODO: 테스트용이지만 비밀번호가 노출된 데이터 세팅. 개선하는 것이 좋을 지 고민해 보자.
-insert into user_account (username, password, nickname, email, memo, created_at, created_by, modified_at, modified_by) values
-('rhkwk3333', 'asdf2063', 'Jung', 'rhkwk@mail.com', 'I am Jung', now(), 'rhkwk3333', now(), 'rhkwk3333');
-insert into user_account (username, password, nickname, email, memo, created_at, created_by, modified_at, modified_by) values
-('iady7777', 'qwerty', 'Blaire', 'iady7777@mail.com', 'I am Blaire', now(), 'iady7777', now(), 'iady7777');
-insert into user_account (username, password, nickname, email, memo, created_at, created_by, modified_at, modified_by) values
-('iamysd629', 'password', 'Umma', 'iamysd@mail.com', 'I am Umma', now(), 'iamysd629', now(), 'iamysd629');
+insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by, modified_at, modified_by) values
+('rhkwk3333', 'asdf2063', 'Jung', 'rhkwk@mail.com', 'Memo1', now(), 'rhkwk3333', now(), 'rhkwk3333');
+insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by, modified_at, modified_by) values
+('iady7777', 'qwerty', 'Blaire', 'iady7777@mail.com', 'Memo2', now(), 'iady7777', now(), 'iady7777');
+insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by, modified_at, modified_by) values
+('iamysd629', 'password', 'Umma', 'iamysd@mail.com', 'Memo3', now(), 'iamysd629', now(), 'iamysd629');
 
 
 -- 123 게시글
