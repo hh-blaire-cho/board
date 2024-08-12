17255024e904b3fd0be00607fb7bb22563ca4b24
blaire <hh-blaire-cho@gmail.com>
2024-08-12 11:49:34 +0900

    #13 Dto Design (게시글,댓글,유저,댓글달린게시글)

---
+++ src/main/java/com/fastcampus/board/dto/CommentDto.java
@@ -0,0 +1,52 @@
+package com.fastcampus.board.dto;
+
+import com.fastcampus.board.domain.Article;
+import com.fastcampus.board.domain.Comment;
+import java.time.LocalDateTime;
+
+public record CommentDto(
+        Long id,
+        Long articleId,
+        UserAccountDto userAccountDto,
+        String content,
+        LocalDateTime createdAt,
+        String createdBy,
+        LocalDateTime modifiedAt,
+        String modifiedBy
+) {
+
+    public static CommentDto of(
+            Long id,
+            Long articleId,
+            UserAccountDto userAccountDto,
+            String content,
+            LocalDateTime createdAt,
+            String createdBy,
+            LocalDateTime modifiedAt,
+            String modifiedBy
+    ) {
+        return new CommentDto(id, articleId, userAccountDto, content, createdAt, createdBy, modifiedAt, modifiedBy);
+    }
+
+    public static CommentDto from(Comment entity) {
+        return new CommentDto(
+                entity.getId(),
+                entity.getArticle().getId(),
+                UserAccountDto.from(entity.getUserAccount()),
+                entity.getContent(),
+                entity.getCreatedAt(),
+                entity.getCreatedBy(),
+                entity.getModifiedAt(),
+                entity.getModifiedBy()
+        );
+    }
+
+    public Comment toEntity(Article entity) {
+        return Comment.of(
+                entity,
+                userAccountDto.toEntity(),
+                content
+        );
+    }
+
+}
---
+++ src/main/java/com/fastcampus/board/dto/UserAccountDto.java
@@ -0,0 +1,59 @@
+package com.fastcampus.board.dto;
+
+import com.fastcampus.board.domain.UserAccount;
+import java.time.LocalDateTime;
+
+public record UserAccountDto(
+        Long id,
+        String username,
+        String password,
+        String email,
+        String nickname,
+        String memo,
+        LocalDateTime createdAt,
+        String createdBy,
+        LocalDateTime modifiedAt,
+        String modifiedBy
+) {
+
+    public static UserAccountDto of(
+            Long id,
+            String userId,
+            String userPassword,
+            String email,
+            String nickname,
+            String memo,
+            LocalDateTime createdAt,
+            String createdBy,
+            LocalDateTime modifiedAt,
+            String modifiedBy
+    ) {
+        return new UserAccountDto(id, userId, userPassword, email, nickname, memo, createdAt, createdBy, modifiedAt, modifiedBy);
+    }
+
+    public static UserAccountDto from(UserAccount entity) {
+        return new UserAccountDto(
+                entity.getId(),
+                entity.getUsername(),
+                entity.getPassword(),
+                entity.getEmail(),
+                entity.getNickname(),
+                entity.getMemo(),
+                entity.getCreatedAt(),
+                entity.getCreatedBy(),
+                entity.getModifiedAt(),
+                entity.getModifiedBy()
+        );
+    }
+
+    public UserAccount toEntity() {
+        return UserAccount.of(
+                username,
+                password,
+                email,
+                nickname,
+                memo
+        );
+    }
+
+}
