db042029c84d76930d9c0dfe129e3c5c051c98b2
Hyeonhui <hh.blaire.cho@gmail.com>
2024-08-13 00:36:44 +0900

    #20 TestHelper created and utilized

--- src/test/java/com/fastcampus/board/service/ArticleServiceTest.java
+++ src/test/java/com/fastcampus/board/service/ArticleServiceTest.java
@@ -1,14 +1,23 @@
 package com.fastcampus.board.service;
 
+import static com.fastcampus.board.TestHelper.USER_ACCOUNT;
+import static com.fastcampus.board.TestHelper.USER_ACCOUNT_DTO;
+import static com.fastcampus.board.TestHelper.createArticleDto;
+import static com.fastcampus.board.TestHelper.randNumb;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.BDDMockito.willDoNothing;
+
 import com.fastcampus.board.domain.Article;
-import com.fastcampus.board.domain.UserAccount;
 import com.fastcampus.board.domain.type.SearchType;
 import com.fastcampus.board.dto.ArticleDto;
 import com.fastcampus.board.dto.ArticleWithCommentsDto;
-import com.fastcampus.board.dto.UserAccountDto;
 import com.fastcampus.board.repository.ArticleRepository;
 import jakarta.persistence.EntityNotFoundException;
-import org.junit.jupiter.api.BeforeEach;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,14 +26,6 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-
-import java.time.LocalDateTime;
-import java.util.Optional;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.*;
 
 @DisplayName("비지니스 로직 - 게시글")
 @ExtendWith(MockitoExtension.class) //이렇게 해야, 굳이 Application 을 안켜서 가벼워짐.
@@ -36,24 +37,12 @@ class ArticleServiceTest {
     @InjectMocks // Mock using Mock, ArticleRepository 를 주입하는 대상, ArticleService 에 의존함 (=사용당함)
     private ArticleService sut; // System Under Test
 
-    private UserAccountDto userAccountDto;
-    private UserAccount userAccount;
-
-    @BeforeEach
-    public void setUp() { //픽스쳐를 만드는 과정
-        userAccountDto = UserAccountDto.of(
-                createRandomId(), "hcho", "password", "hcho@mail.com", "winkyhcho", "This is memo",
-                LocalDateTime.now(), "hcho", LocalDateTime.now(), "hcho");
-
-        userAccount = userAccountDto.toEntity();
-    }
-
     @DisplayName("아이디로 게시글 검색 시, 해당 게시글 반환")
     @Test
     void test_getArticlesUsingArticleId() {
         // Given articleId
-        long articleId = createRandomId();
-        Article article = createArticle("random1", "random2", "random3");
+        long articleId = randNumb();
+        Article article = Article.of(USER_ACCOUNT, "random1", "random2", "random3");
         given(articleRepo.findById(articleId)).willReturn(Optional.of(article));
 
         // When searching it
@@ -72,7 +61,7 @@ class ArticleServiceTest {
     @Test
     void test_getArticlesUsingWrongArticleId() {
         // Given wrong articleId
-        long articleId = createRandomId();
+        long articleId = randNumb();
         given(articleRepo.findById(articleId)).willReturn(Optional.empty());
 
         // When searching it
@@ -104,7 +93,7 @@ class ArticleServiceTest {
     @Test
     void test_savingArticle() {
         // Given article info
-        ArticleDto dto = createArticleDto("title", "content", "#tag");
+        ArticleDto dto = createArticleDto();
         given(articleRepo.save(any(Article.class))).willReturn(null);
 
         // When try saving it
@@ -118,8 +107,8 @@ class ArticleServiceTest {
     @Test
     void test_updatingArticle() {
         // Given updated info with original
-        Article original = createArticle("title", "content", "#tag");
-        ArticleDto updated = createArticleDto("newtitle", "newcontent", "#newtag");
+        Article original = Article.of(USER_ACCOUNT, "title", "content", "#tag");
+        ArticleDto updated = createArticleDto(USER_ACCOUNT_DTO, "newtitle", "newcontent", "#newtag");
         given(articleRepo.getReferenceById(updated.id())).willReturn(original);
 
         // When try updating it
@@ -138,7 +127,7 @@ class ArticleServiceTest {
     @Test
     void test_updatingNonExistedArticle() {
         // Given updated info without original
-        ArticleDto updated = createArticleDto("newtitle", "newcontent", "#newtag");
+        ArticleDto updated = createArticleDto();
         given(articleRepo.getReferenceById(updated.id())).willThrow(EntityNotFoundException.class);
 
         // When try updating it
@@ -152,7 +141,7 @@ class ArticleServiceTest {
     @Test
     void test_deletingArticle() {
         // Given article id
-        long articleId = createRandomId();
+        long articleId = randNumb();
         willDoNothing().given(articleRepo).deleteById(articleId);
 
         // When try deleting using that id
@@ -160,20 +149,6 @@ class ArticleServiceTest {
 
         // Then should delete it properly
         then(articleRepo).should().deleteById(articleId);
-    }
-
-
-    private ArticleDto createArticleDto(String title, String content, String hashtag) {
-        return ArticleDto.of(1L, userAccountDto, title, content, hashtag,
-                LocalDateTime.now(), "hcho", LocalDateTime.now(), "hcho");
-    }
-
-    private Article createArticle(String title, String content, String hashtag) {
-        return Article.of(userAccount, title, content, hashtag);
-    }
-
-    private long createRandomId() {
-        return (long) (Math.random() * 99);
     }
 
 }
