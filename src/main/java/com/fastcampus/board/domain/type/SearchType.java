4fd206724ebfacfe253da8a4e0216e9661d697a3
Blaire <hh.blaire.cho@gmail.com>
2024-08-17 11:14:28 +0900

    #29 add description-string to SearchType

--- src/main/java/com/fastcampus/board/domain/type/SearchType.java
+++ src/main/java/com/fastcampus/board/domain/type/SearchType.java
@@ -1,6 +1,19 @@
 package com.fastcampus.board.domain.type;
 
+import lombok.Getter;
+
 //검색어 종류
+@Getter
 public enum SearchType {
-    TITLE, CONTENT, USERNAME, NICKNAME, HASHTAG
+    TITLE("제목"),
+    CONTENT("본문"),
+    USERNAME("유저네임"),
+    NICKNAME("닉네임"),
+    HASHTAG("해시태그");
+
+    private final String description;
+
+    SearchType(String description) {
+        this.description = description;
+    }
 }
