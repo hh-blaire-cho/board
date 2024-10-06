b8cd8e96480654b74215b4f293ee8d992d7e461b
Blaire <hh.blaire.cho@gmail.com>
2024-10-06 18:37:01 +0900

    #59 댓글 좋아요 서비스 & 컨트롤러 & 리스폰스

--- src/main/java/com/fastcampus/board/service/CommentService.java
+++ src/main/java/com/fastcampus/board/service/CommentService.java
@@ -2,13 +2,17 @@ package com.fastcampus.board.service;
 
 import com.fastcampus.board.domain.Article;
 import com.fastcampus.board.domain.Comment;
+import com.fastcampus.board.domain.Like;
 import com.fastcampus.board.domain.UserAccount;
 import com.fastcampus.board.dto.CommentDto;
+import com.fastcampus.board.dto.LikeDto;
 import com.fastcampus.board.repository.ArticleRepository;
 import com.fastcampus.board.repository.CommentRepository;
+import com.fastcampus.board.repository.LikeRepository;
 import com.fastcampus.board.repository.UserAccountRepository;
 import jakarta.persistence.EntityNotFoundException;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -23,20 +27,21 @@ public class CommentService {
     private final ArticleRepository articleRepository;
     private final CommentRepository commentRepository;
     private final UserAccountRepository userAccountRepository;
+    private final LikeRepository likeRepository;
 
     @Transactional(readOnly = true)
     public List<CommentDto> searchComments(Long articleId) { //댓글 고유 아이디가 아니라, 상위 게시글 아이디. 서비스에서는 고유 아이디로 댓글을 검색하지 않음
         return commentRepository.findByArticle_Id(articleId)
-                .stream()
-                .map(CommentDto::from)
-                .toList();
+            .stream()
+            .map(CommentDto::from)
+            .toList();
     }
 
     @Transactional(readOnly = true)
     public CommentDto getComment(Long commentId) {
         return commentRepository.findById(commentId)
-                .map(CommentDto::from)
-                .orElseThrow(() -> new EntityNotFoundException("Cannot find that comment with given id : " + commentId));
+            .map(CommentDto::from)
+            .orElseThrow(() -> new EntityNotFoundException("Cannot find that comment with given id : " + commentId));
     }
 
 
@@ -83,6 +88,37 @@ public class CommentService {
 
     public void deleteComment(Long commentId, String username) {
         commentRepository.deleteByIdAndUserAccount_Username(commentId, username);
+    }
+
+    @Transactional
+    public void toggleLike(Long commentId, String username) {
+        Comment comment = commentRepository.findById(commentId).orElseThrow(() ->
+            new RuntimeException(String.format("cannot find comment id: %d", commentId)));
+
+        UserAccount userAccount = userAccountRepository.findByUsername(username).orElseThrow(() ->
+            new RuntimeException(String.format("cannot find user name: %d", commentId)));
+
+        Optional<Like> likeEntity = likeRepository.findByUserAccountAndComment(userAccount, comment);
+        if (likeEntity.isPresent()) {
+            likeRepository.delete(likeEntity.get());
+        } else {
+            likeRepository.save(Like.createForComment(userAccount, comment));
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public List<LikeDto> getLikes(Long commentId) {
+        Comment comment = commentRepository.findById(commentId).orElseThrow(() ->
+            new RuntimeException(String.format("cannot find comment id: %d", commentId)));
+
+        return likeRepository.findAllByComment(comment).stream().map(LikeDto::from).toList();
+    }
+
+    public int getLikeCount(Long commentId) {
+        Comment comment = commentRepository.findById(commentId).orElseThrow(() ->
+            new RuntimeException(String.format("cannot find comment id: %d", commentId)));
+
+        return likeRepository.countByComment(comment);
     }
 
 }
--- src/test/java/com/fastcampus/board/service/LikeServiceTest.java
+++ src/test/java/com/fastcampus/board/service/LikeServiceTest.java
@@ -1,6 +1,7 @@
 package com.fastcampus.board.service;
 
 import static com.fastcampus.board.TestHelper.USER_ACCOUNT_DTO;
+import static com.fastcampus.board.TestHelper.randNumb;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -10,9 +11,11 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.fastcampus.board.domain.Article;
+import com.fastcampus.board.domain.Comment;
 import com.fastcampus.board.domain.Like;
 import com.fastcampus.board.domain.UserAccount;
 import com.fastcampus.board.repository.ArticleRepository;
+import com.fastcampus.board.repository.CommentRepository;
 import com.fastcampus.board.repository.LikeRepository;
 import com.fastcampus.board.repository.UserAccountRepository;
 import java.util.Optional;
@@ -32,6 +35,12 @@ class LikeServiceTest {
     @MockBean
     private ArticleRepository articleRepo;
 
+    @Autowired
+    CommentService commentService;
+
+    @MockBean
+    private CommentRepository commentRepo;
+
     @MockBean
     private UserAccountRepository userRepo;
 
@@ -41,61 +50,91 @@ class LikeServiceTest {
 
     @Test
     @DisplayName("좋아요 추가 정상 케이스")
-    void test_toggleLikePost_addLike() {
-        Long articleId = 1L;
+    void test_toggleLike_addLike() {
+        Long articleId = randNumb();
+        Long commentId = randNumb();
         String username = "randomUsername";
         UserAccount userAccount = USER_ACCOUNT_DTO.toEntity();
         Article article = Article.of(userAccount, "title", "content", "#hash");
+        Comment comment = Comment.of(article, userAccount, "content");
         when(articleRepo.findById(articleId)).thenReturn(Optional.of(article));
+        when(commentRepo.findById(commentId)).thenReturn(Optional.of(comment));
         when(userRepo.findByUsername(username)).thenReturn(Optional.of(userAccount));
         when(likeRepo.findByUserAccountAndArticle(userAccount, article)).thenReturn(Optional.empty());
+        when(likeRepo.findByUserAccountAndComment(userAccount, comment)).thenReturn(Optional.empty());
 
         assertDoesNotThrow(() -> articleService.toggleLike(articleId, username));
-        verify(likeRepo, times(1)).save(any(Like.class));
+        assertDoesNotThrow(() -> commentService.toggleLike(commentId, username));
+        verify(likeRepo, times(2)).save(any(Like.class));
     }
 
     @Test
-    @DisplayName("좋아요 취소 정상 케이스")
-    void test_toggleLikePost_removeLike() {
+    @DisplayName("좋아요 취소 정상 케이스 - 게시글")
+    void test_toggleLikeArticle_removeLike() {
         Long articleId = 1L;
         String username = "randomUsername";
         UserAccount userAccount = USER_ACCOUNT_DTO.toEntity();
         Article article = Article.of(userAccount, "title", "content", "#hash");
-        Like likeEntity = Like.createForArticle(userAccount, article);
+        Like like = Like.createForArticle(userAccount, article);
 
         when(articleRepo.findById(articleId)).thenReturn(Optional.of(article));
         when(userRepo.findByUsername(username)).thenReturn(Optional.of(userAccount));
-        when(likeRepo.findByUserAccountAndArticle(userAccount, article)).thenReturn(Optional.of(likeEntity));
+        when(likeRepo.findByUserAccountAndArticle(userAccount, article)).thenReturn(Optional.of(like));
 
         assertDoesNotThrow(() -> articleService.toggleLike(articleId, username));
-        verify(likeRepo, times(1)).delete(likeEntity);
+        verify(likeRepo, times(1)).delete(like);
     }
 
     @Test
-    @DisplayName("좋아요 개수 조회 정상 케이스")
-    void test_getLikeCountOfArticle() {
+    @DisplayName("좋아요 취소 정상 케이스 - 댓글")
+    void test_toggleLikeComment_removeLike() {
         Long articleId = 1L;
+        Long commentId = randNumb();
+        String username = "randomUsername";
         UserAccount userAccount = USER_ACCOUNT_DTO.toEntity();
         Article article = Article.of(userAccount, "title", "content", "#hash");
+        Comment comment = Comment.of(article, userAccount, "content");
+        Like like = Like.createForComment(userAccount, comment);
+
+        when(commentRepo.findById(commentId)).thenReturn(Optional.of(comment));
+        when(userRepo.findByUsername(username)).thenReturn(Optional.of(userAccount));
+        when(likeRepo.findByUserAccountAndComment(userAccount, comment)).thenReturn(Optional.of(like));
+
+        assertDoesNotThrow(() -> commentService.toggleLike(commentId, username));
+        verify(likeRepo, times(1)).delete(like);
+    }
+
+    @Test
+    @DisplayName("좋아요 개수 조회 정상 케이스 - 게시글")
+    void test_getLikeCountOfArticle() {
+        Long articleId = randNumb();
+        UserAccount userAccount = USER_ACCOUNT_DTO.toEntity();
+        String username = userAccount.getUsername();
+        Article article = Article.of(userAccount, "title", "content", "#hash");
+        int originalLikeCount = (int) randNumb();
 
         when(articleRepo.findById(articleId)).thenReturn(Optional.of(article));
-        when(likeRepo.countByArticle(article)).thenReturn(99);
-        int likeCount = articleService.getLikeCount(articleId);
-        assertEquals(99, likeCount);
+        when(userRepo.findByUsername(username)).thenReturn(Optional.of(userAccount));
+        when(likeRepo.countByArticle(article)).thenReturn(originalLikeCount);
+        assertEquals(originalLikeCount, articleService.getLikeCount(articleId));
     }
 
+
     @Test
-    @DisplayName("좋아요 추가 시 게시글이 없으면 에러")
-    void test_toggleLikePost_withoutPost() {
-        Long articleId = 1L;
-        String username = "randomUsername";
+    @DisplayName("좋아요 토글 시 게시글이나 댓글이 없으면 에러")
+    void test_toggleLike_withoutArticleOrComment() {
+        Long articleId = randNumb();
+        Long commentId = randNumb();
+        String username = "tstUser";
         when(articleRepo.findById(articleId)).thenReturn(Optional.empty());
+        when(commentRepo.findById(articleId)).thenReturn(Optional.empty());
         assertThrows(RuntimeException.class, () -> articleService.toggleLike(articleId, username));
+        assertThrows(RuntimeException.class, () -> commentService.toggleLike(commentId, username));
     }
 
     @Test
-    @DisplayName("좋아요 추가 시 유저가 없으면 에러")
-    void test_toggleLikePost_withoutUser() {
+    @DisplayName("좋아요 토글 시 유저가 없으면 에러")
+    void test_toggleLike_withoutUser() {
         Long articleId = 1L;
         UserAccount userAccount = USER_ACCOUNT_DTO.toEntity();
         Article article = Article.of(userAccount, "random1", "random2", "random3");
@@ -106,10 +145,13 @@ class LikeServiceTest {
     }
 
     @Test
-    @DisplayName("좋아요 개수 조회 시 게시글이 없으면 에러")
-    void test_getLikeCount_OfArticle_withoutPost() {
-        Long articleId = 1L;
+    @DisplayName("좋아요 개수 조회 시 게시글이나 댓글이 없으면 에러")
+    void test_getLikeCount_withoutArticleOrComment() {
+        Long articleId = randNumb();
+        Long commentId = randNumb();
         when(articleRepo.findById(articleId)).thenReturn(Optional.empty());
+        when(commentRepo.findById(commentId)).thenReturn(Optional.empty());
         assertThrows(RuntimeException.class, () -> articleService.getLikeCount(articleId));
+        assertThrows(RuntimeException.class, () -> commentService.getLikeCount(commentId));
     }
 }
